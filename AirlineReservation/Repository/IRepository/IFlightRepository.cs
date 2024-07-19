12bab54b6f99c8b2ba7a58732aa95af358086cdb
Mohammed Haseeb <mdhaseeb.98@gmail.com>
2024-07-19 23:05:55 +0530

    Update the Flight Repository

--- AirlineReservation/Repository/IRepository/IFlightRepository.cs
+++ AirlineReservation/Repository/IRepository/IFlightRepository.cs
@@ -5,7 +5,8 @@ namespace AirlineReservation.Repository.IRepository
     public interface IFlightRepository
     {
         Task<IEnumerable<Flight>> GetAllFlightsAsync();
-        Task<Flight> GetFlight(int flightId);
+        Task<Flight> GetFlightByIdAsync(int flightId);
+        Task<Flight> GetFlightByNumberAsync(string flightNumber);
         Task AddFlightAsync(Flight flight);
         Task UpdateFlightAsync(Flight flight);
         Task DeleteFlightAsync(int flightId);
