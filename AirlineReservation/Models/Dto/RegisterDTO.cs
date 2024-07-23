da29b74701db6b8f42bbbd6e34e58fbd81d590d8
Mohammed Haseeb <mdhaseeb.98@gmail.com>
2024-07-23 16:29:32 +0530

    Updated models and added dtos

---
+++ AirlineReservation/Models/Dto/RegisterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AirlineReservation.Models.Dto
+{
+    public class RegisterDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
